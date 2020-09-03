df3d2e143954e1f4aa7881c1017b022e324c1748
Carolina Procaccini <cprocaccini@ciandt.com>
2020-09-03 09:15:51 -0300

    Readme, gitignore and pom files updated and added new package and class

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # curseriaServiceTesting
 Service test automation framework - Using Java + Junit
+
+## Requirements  
+1 - JDK > 1.8  
+2 - Maven project  
---
+++ src/test/java/test/TestClass.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.junit.Test;
+
+public class TestClass {
+    int nota1;
+    int nota2;
+
+    @Test
+    public void media(){
+
+    }
+
+    @Test
+    public void tempo(){
+
+    }
+
+}
