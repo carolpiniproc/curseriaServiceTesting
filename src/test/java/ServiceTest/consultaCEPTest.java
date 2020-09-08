fc6827bb85ddd525db9900538e356e8b1a413684
carolpiniproc <39374439+carolpiniproc@users.noreply.github.com>
2020-09-08 09:00:53 -0300

    Created new class file for RestAssured test.

--- src/test/java/ServiceTest/consultaCEPTest.java
+++ src/test/java/ServiceTest/consultaCEPTest.java
@@ -1,14 +1,28 @@
 package ServiceTest;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.Test;
 
 public class consultaCEPTest {
+
+    String url = "http://viacep.com.br/ws/";
+
     @Test
     public void consultaCEP(){
        //http://viacep.com.br/ws/{{cep}}/json/
         String cep = "13087500";
-        String url = "http://viacep.com.br/ws/";
         String endpoint = cep.concat("/json/");
+
+        RestAssured.baseURI = url;
+
+        RestAssured.given()
+                .relaxedHTTPSValidation()
+                .contentType(ContentType.JSON)
+                .given()
+                .get(endpoint)
+                .then()
+                .statusCode(200);
     }
 }
 
