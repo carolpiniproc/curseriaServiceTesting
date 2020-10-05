f996103599a749b19554da70dd8a04bd51dacf4a
carolpiniproc <39374439+carolpiniproc@users.noreply.github.com>
2020-10-05 01:54:14 -0300

    Added API tests from CEP.
    Signed-off-by: Carolina Procaccini <cprocaccini@ciandt.com>

--- src/test/java/serviceTest/consultaCEPTest.java
+++ src/test/java/serviceTest/consultaCEPTest.java
@@ -4,62 +4,43 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.junit.Test;
+
 import java.util.LinkedHashMap;
+
 import static org.junit.Assert.assertEquals;
 
 public class consultaCEPTest {
+    //http://viacep.com.br/ws/{{cep}}/json/
     String url = "http://viacep.com.br/ws/";
 
     @Test
-    public void consultaCEPParams(){
-       //http://viacep.com.br/ws/{{cep}}/json/
+    public void validaStatusCode() {
         String cep = "13087500";
-        String endpoint = cep.concat("/json/");
-        LinkedHashMap<String, String> param = new LinkedHashMap<>();
+        String endpoint = cep.concat("/json/"); // ou pode usar endpoint = cep+"/json/";
 
         RestAssured.baseURI = url;
-
-        Response response = (Response) RestAssured.given()
-                .relaxedHTTPSValidation()
+        RestAssured.given()
+                .relaxedHTTPSValidation() //SSL para sites com certificado não válido
                 .contentType(ContentType.JSON)
-                .params(param)
                 .when()
                 .get(endpoint)
                 .then()
-                .statusCode(200)
-                .extract()
-                .response();
-
-        JsonPath json = response.getBody().jsonPath();
-
-        assertEquals("Rua Hermantino Coelho", json.get("logradouro"));
+                .statusCode(200);
     }
 
     @Test
-    public void validaDadosCEP(){
+    public void validaDadosCEPHeaders(){
         String cep = "13087500";
         String endpoint = cep.concat("/json/");
         LinkedHashMap<String, String> header = new LinkedHashMap<>();
         header.put("clientId", "curso");
-        header.put("Authorization", "Basic");
-    }
-
-
-    @Test
-    public void consultaCEPHeaders(){
-        //http://viacep.com.br/ws/{{cep}}/json/
-        String cep = "13087500";
-        String endpoint = cep.concat("/json/");
-        LinkedHashMap<String, String> header = new LinkedHashMap<>();
-        header.put("clientId", "curso");
-        header.put("Authorization", "Basic");
+        header.put("Authorization", "Basic Y2Fyb2w6dGVzdGU=");
 
         RestAssured.baseURI = url;
 
-        Response response = (Response) RestAssured.given()
-                .relaxedHTTPSValidation()
+        Response response = RestAssured.given()
+                .relaxedHTTPSValidation() //SSL para sites com certificado não válido
                 .contentType(ContentType.JSON)
                 .headers(header)
                 .when()
@@ -71,12 +52,42 @@ public class consultaCEPTest {
 
         JsonPath json = response.getBody().jsonPath();
 
+        assertEquals("Mansões Santo Antônio", json.get("bairro"));
         assertEquals("Rua Hermantino Coelho", json.get("logradouro"));
+        assertEquals("Campinas", json.get("localidade"));
     }
 
 
+    @Test
+    public void validaDadosCEPParam(){
+        String cep = "13087500";
+        String endpoint = cep.concat("/json/"); // ou pode usar endpoint = cep+"/json/";
+        LinkedHashMap<String, String> param = new LinkedHashMap<>();
+        param.put("clientId", "curso");
+        param.put("nome", "Carol");
 
+        RestAssured.baseURI = url;
 
+        Response response = (Response) RestAssured.given()
+                .relaxedHTTPSValidation()
+                .contentType(ContentType.JSON)
+//                .param(parameterName, parameterValue)
+//                .param("clientId", "curso")
+//                .param("nome", "Carol")
+                .params(param)
+                .when()
+                .get(endpoint)
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        JsonPath json = response.getBody().jsonPath();
+
+        assertEquals("Mansões Santo Antônio", json.get("bairro"));
+        assertEquals("Rua Hermantino Coelho", json.get("logradouro"));
+        assertEquals("Campinas", json.get("localidade"));
+    }
 
 }
 
